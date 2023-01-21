de0e7f39d81e5aabdd948e843130f6f30c1ce93b
Ult1mat3S <52617311+Ult1mat3S@users.noreply.github.com>
2023-01-21 02:09:39 -0600

    fix: fix copypaste typo

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Ult1mat3S/meme-generator.git main:gh-pages
+git push -f git@github.com:Ult1mat3S/tenzies.git main:gh-pages
 
 cd -
