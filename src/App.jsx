08bd56ae6fc3212d3ffe60af3ef925b0e04c4e9c
Ult1mat3S <52617311+Ult1mat3S@users.noreply.github.com>
2023-02-07 02:06:29 -0600

    feat: add styling to buttons, finish screen animation, ability to reset game

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,86 @@
-import { useState } from "react";
+import { nanoid } from "nanoid";
+import { useEffect, useState } from "react";
+import Confetti from "react-confetti";
 import DieComponent from "./components/DieComponent";
 
 export default function App() {
   const [diceState, setDiceState] = useState(allNewDice());
+  const [tenzies, setTenzies] = useState(true);
+
+  useEffect(() => {
+    const allHeld = diceState.every((diceState) => diceState.isHeld);
+    const firstValue = diceState[0].value;
+    const allSameValue = diceState.every(
+      (diceState) => diceState.value === firstValue
+    );
+    if (allHeld && allSameValue) {
+      setTenzies(true);
+    }
+  }, [diceState]);
+
+  function generateNewDie() {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid(),
+    };
+  }
 
   function allNewDice() {
     const newDice = [];
     for (let i = 0; i < 10; i++) {
-      newDice.push(Math.ceil(Math.random() * 6));
+      newDice.push(generateNewDie());
     }
     return newDice;
   }
 
-  const diceElements = diceState.map((num, index) => (
-    <DieComponent value={num} key={index} />
+  function holdDice(id) {
+    setDiceState((prevDice) =>
+      prevDice.map((diceElement) => {
+        return diceElement.id === id
+          ? { ...diceElement, isHeld: !diceElement.isHeld }
+          : diceElement;
+      })
+    );
+  }
+
+  function resetGame() {
+    setTenzies(false);
+    setDiceState(allNewDice());
+  }
+
+  function rollDice() {
+    if (tenzies == true) {
+      resetGame();
+    }
+    setDiceState((prevDiceRoll) =>
+      prevDiceRoll.map((diceElement) => {
+        return diceElement.isHeld ? diceElement : generateNewDie();
+      })
+    );
+  }
+
+  const diceElements = diceState.map((diceElement) => (
+    <DieComponent
+      value={diceElement.value}
+      key={diceElement.id}
+      isHeld={diceElement.isHeld}
+      holdDice={() => holdDice(diceElement.id)}
+    />
   ));
 
   return (
     <main>
-      <div className="dice-container">{diceElements}</div>
+      {tenzies && <Confetti />}
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
+      <div className="dice-container">{diceElements} </div>
+      <button className="dice-button" onClick={rollDice}>
+        {tenzies ? "New Game" : "Roll"}
+      </button>
     </main>
   );
 }
--- src/components/DieComponent.jsx
+++ src/components/DieComponent.jsx
@@ -1,6 +1,10 @@
 export default function DieComponent(props) {
+  const styles = {
+    backgroundColor: props.isHeld ? "#59e391" : "white",
+  };
+
   return (
-    <div className="die-face">
+    <div className="die-face" style={styles} onClick={props.holdDice}>
       <h2 className="die-number">{props.value}</h2>
     </div>
   );
